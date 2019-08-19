b7f92ca51306c7b8dd8435152e579537ca9a5425
noteday <1224001817@qq.com>
2019-08-19 17:34:33 +0800

    成功爬去所有通知

--- 1.py
+++ 1.py
@@ -1,6 +1,6 @@
 import requests
-
 from bs4 import BeautifulSoup
+import csv
 
 
 def get_url(url, url_list):
@@ -11,12 +11,12 @@ def get_url(url, url_list):
     # r.text获得文本，r.content获得文件
     soup = BeautifulSoup(r.text, 'html.parser')
     links = soup.find_all('li')
-    print(links)
+    #print(links)
     main_url = 'http://www.gzhu.edu.cn/info/1185/'
 
     for link in links:
         a = link.a['href']
-        print(a)
+        #print(a)
         b = str(a)
         c = b.split("/")
         if len(b.split("/")) >= 4:
@@ -29,23 +29,47 @@ def get_url(url, url_list):
             if c[index].endswith('.htm'):
                 url_list.append(main_url + c[index])
 
-    print(url_list)
+   # print(url_list)
     return
 
+#得到每一条通知的内容
+def get_content(url_list):
+    with open('test1.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(('标题', '稿件来源', '内容'))
+    for url in url_list:
+        print(url)
+        r = requests.get(url=url)
+        r.encoding = 'utf-8'
+        soup = BeautifulSoup(r.text, 'html.parser')
+        title = soup.find('p', class_="neirong-main-con-right-bottom-bt1").text
+        print(title)
+
+        message = soup.find('p', class_="neirong-main-con-right-bottom-bt2").text
+        print(message)
+
+        content = soup.find('div', id="vsb_content").text
+        print(content)
+        #pub_data = soup.find('span', class_="Article_PublishDate").text
+        #current_time = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+
+        with open('test1.csv', 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow((title, message, content))
 
 def main():
-    SUM = 1
+    SUM = 27
     url_list = []
     while SUM != 0:
         if SUM == 27:
             get_url("http://www.gzhu.edu.cn/index/tzgg/27.htm", url_list)
         if SUM != 27:
             get_url("http://www.gzhu.edu.cn/index/tzgg/" + str(SUM) + ".htm", url_list)
-
         SUM = SUM - 1
     print(url_list)
     print(len(url_list))
 
+    get_content(url_list)
 
     return
 
